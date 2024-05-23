39f35076f50e2c7c125c6412bce23ab85fa51ede
Nathan Hutchison <nathanhutchison02@gmail.com>
2024-05-23 17:52:47 +0100

    feat(project): added loading animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
-
+import Loading from "./components/Loading.jsx";
 
 function App() {
   return (
-    <div 
-      className="h-screen bg-slate-900 w-screen">
-        
+    <>
+      <div className="relative bg-zinc-900">
+        <div className="absolute w-full h-fit">
+          <h1 className="text-white">Home Page</h1>
+        </div>
+        <Loading className='bg-zinc-900 absolute' color={"#b54400"} />
       </div>
+    </>
   );
 }
 
