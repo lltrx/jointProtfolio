937b22d45596782dbc737494e13f0255cedd38ce
Turki Faisal <Turkif182@gmail.com>
2024-05-23 12:21:25 +0100

    build(project): build a react app with tailwind css
    Closes #1

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-slate-600">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
