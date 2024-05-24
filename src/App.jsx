97c569cf7a14f93b293e26c4d6065d29b5d4e04a
Nathan Hutchison <nathanhutchison02@gmail.com>
2024-05-24 21:36:55 +0100

    feat(project): added placeholder landing text and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Loading from "./components/Loading.jsx";
+import Home from "./components/Home.jsx";
 
 function App() {
   
@@ -7,23 +8,8 @@ function App() {
   return (
     <>
       <div className="relative bg-zinc-900">
-        <div className="absolute bg-zinc-900 w-full h-fit">
-          <h1 className="text-4xl text-center text-white pt-20">Welcome to the App</h1>
-          <h1 className="text-4xl text-center text-white pt-20">Welcome to the App</h1>
-          <h1 className="text-4xl text-center text-white pt-20">Welcome to the App</h1>
-          <h1 className="text-4xl text-center text-white pt-20">Welcome to the App</h1>
-          <h1 className="text-4xl text-center text-white pt-20">Welcome to the App</h1>
-          <h1 className="text-4xl text-center text-white pt-20">Welcome to the App</h1>
-          <h1 className="text-4xl text-center text-white pt-20">Welcome to the App</h1>
-          <h1 className="text-4xl text-center text-white pt-20">Welcome to the App</h1>
-          <h1 className="text-4xl text-center text-white pt-20">Welcome to the App</h1>
-          <h1 className="text-4xl text-center text-white pt-20">Welcome to the App</h1>
-          <h1 className="text-4xl text-center text-white pt-20">Welcome to the App</h1>
-          <h1 className="text-4xl text-center text-white pt-20">Welcome to the App</h1>
-          <h1 className="text-4xl text-center text-white pt-20">Welcome to the App</h1>
-          <h1 className="text-4xl text-center text-white pt-20">Welcome to the App</h1>
-          <h1 className="text-4xl text-center text-white pt-20">Welcome to the App</h1>
-          <h1 className="text-4xl text-center text-white pt-20">Welcome to the App</h1>
+        <div className="absolute flex flex-col bg-zinc-900 w-full h-fit items-center">
+          <Home />
         </div>
         <Loading className='bg-zinc-900 absolute' color={"#b54400"} />
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'display': ['Poppins', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
