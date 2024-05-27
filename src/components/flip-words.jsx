d520124f9236476268f1c714676983c20e76ae41
Turki Faisal <Turkif182@gmail.com>
2024-05-27 12:36:14 +0100

    feat(flip word): add the flip word component
    #3

---
+++ src/components/flip-words.jsx
@@ -0,0 +1,80 @@
+/* eslint-disable no-undef */
+/* eslint-disable react/prop-types */
+import{ useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import {cn} from "../utils/cn";
+
+export const FlipWords = ({ words, duration = 3000, className }) => {
+  const [currentWord, setCurrentWord] = useState(words[0]);
+  let interval;
+
+  useEffect(() => {
+    startAnimation();
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const startAnimation = () => {
+    let i = 0;
+    interval = setInterval(() => {
+      i++;
+      if (i === words.length) {
+        i = 0;
+      }
+      const word = words[i];
+      setCurrentWord(word);
+    }, duration);
+  };
+
+  return (
+    <AnimatePresence>
+      <motion.div
+        initial={{
+          opacity: 0,
+          y: 10,
+        }}
+        animate={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          duration: 0.4,
+          ease: "easeInOut",
+          type: "spring",
+          stiffness: 100,
+          damping: 10,
+        }}
+        exit={{
+          opacity: 0,
+          y: -40,
+          x: 40,
+          filter: "blur(8px)",
+          scale: 2,
+          position: "absolute",
+        }}
+        className={cn(
+          "z-10 inline-block relative text-left text-neutral-900 dark:text-neutral-100",
+          className
+        )}
+        key={currentWord}
+      >
+        {currentWord.split("").map((letter, index) => (
+          <motion.span
+            key={currentWord + index}
+            initial={{ opacity: 0, y: 10, filter: "blur(8px)" }}
+            animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
+            transition={{
+              delay: index * 0.08,
+              duration: 0.4,
+            }}
+            className="inline-block"
+          >
+            {letter}
+          </motion.span>
+        ))}
+      </motion.div>
+    </AnimatePresence>
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,21 @@
-
+import { FlipWords } from "../components/flip-words";
 
 export default function Home() {
-    return (
-        <div className="flex flex-col w-2/3 items-left pt-60 p-20 space-y-4 h-screen">
-            <h1 className="text-orange-500 text-xl font-display">Hi, my name is</h1>
-            <h1 className="text-slate-300 text-7xl font-bold font-display">Nathan Hutchison.</h1>
-            <h1 className="text-orange-900 text-7xl font-bold font-display">I build things for the web.</h1>
-            <p className="text-slate-200 text-lg w-3/4 font-display">I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible, human-centered products at Upstatement.</p>
-        </div>
-    );
+  const words = ["design", "develop", "deploy"];
+  return (
+    <div className="flex flex-col w-2/3 items-left pt-60 p-20 space-y-4 h-screen">
+      <h1 className="text-orange-500 text-xl font-display">Hi, my name is</h1>
+      <h1 className="text-slate-300 text-7xl font-bold font-display">
+        Nathan Hutchison.
+      </h1>
+      <h2 className="text-slate-200 text-3xl font-display flex gap-2">
+        I <FlipWords words={words} duration={2000} className="ml-4" />
+      </h2>
+      <p className="text-slate-200 text-lg w-3/4 font-display">
+        I’m a software engineer specializing in building (and occasionally
+        designing) exceptional digital experiences. Currently, I’m focused on
+        building accessible, human-centered products at Upstatement.
+      </p>
+    </div>
+  );
 }
-
