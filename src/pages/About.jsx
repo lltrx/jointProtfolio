e2060737056d0ecea56f820491640d0ba67dda8c
Nathan Hutchison <nathanhutchison02@gmail.com>
2024-05-27 16:46:36 +0100

    feat(about): changed layout to include image

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,12 +3,12 @@ import { motion } from "framer-motion";
 
 export default function About() {
     return (
-        <div className="flex flex-col items-center pt-60 p-20 space-y-4 h-screen">
-            <div className="z-10 w-full flex justify-center">
+        <div className="flex flex-col items-center p-20 space-y-4 h-screen">
+            <div className="z-10 w-full flex flex-col justify-center">
                 <LampContainer>
                         <motion.h1
-                        initial={{ opacity: 0.5, y: 100 }}
-                        whileInView={{ opacity: 1, y: -50 }}
+                        initial={{ opacity: 0.5, y: 50 }}
+                        whileInView={{ opacity: 1, y: -300 }}
                         transition={{
                         delay: 0.3,
                         duration: 0.8,
@@ -17,21 +17,24 @@ export default function About() {
                         className="mt-8 bg-gradient-to-br from-orange-900 to-orange-500 py-4 bg-clip-text text-center text-4xl font-display tracking-tight text-transparent md:text-7xl"
                     >
                         About Me
-                    </motion.h1>
-                    <motion.p
-                        initial={{ opacity: 0.5, y: 100 }}
-                        whileInView={{ opacity: 1, y: -50 }}
+                    </motion.h1>   
+                    <motion.div 
+                        className="flex"
+                        initial={{ opacity: 0.5, y: 50 }}
+                        whileInView={{ opacity: 1, y: -250 }}
                         transition={{
-                        delay: 0.4,
+                        delay: 0.3,
                         duration: 0.8,
                         ease: "easeInOut",
                         }}
-                        className="mt-8 text-lg text-center text-slate-200 font-display w-1/2"
                     >
-                        I'm a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible, human-centered products at Upstatement.
-                    </motion.p> 
-                    
+                        <p className="mt-8 text-lg text-center text-slate-200 font-display w-1/2">
+                            I'm a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible, human-centered products at Upstatement.
+                        </p> 
+                        <img src="/profile.png" className="w-[200px] rounded-full mx-auto" />
+                    </motion.div>                
                 </LampContainer>
+                
             </div>
         </div>
     );
