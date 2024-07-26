f15bf3489b454a7a6d6292e4e64737a027b30a91
Nathan Hutchison <nathanhutchison02@gmail.com>
2024-07-26 17:15:46 +0100

    fix(about): adjusted vertical alignment of About page for laptop screen size

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,8 @@
 import { LampContainer } from "../components/lamp";
 import { LampContainerSmall } from "../components/lampSmall";
 import { useState, useEffect } from "react"
+import { WavingHand } from '@tensaye-yuan/react-waving-hand'
+import '@tensaye-yuan/react-waving-hand/dist/index.css'
 
 
 export default function About() {
@@ -35,7 +37,7 @@ export default function About() {
 
 
     return (
-        <div className="flex flex-col w-full items-center mb-48 md:p-20 space-y-4 h-screen">
+        <div className="flex flex-col w-full items-center mb-48 md:p-20 lg:pt-0 2xl:pt-20 lg:-mt-[75px] 2xl:mt-0 space-y-4 h-screen">
             <div className="z-10 w-full flex flex-col justify-center items-center">
                 {
                     dark ? (
@@ -46,9 +48,9 @@ export default function About() {
                                         About Me
                                     </h1>      
                                 </LampContainer>
-                                <div className="flex w-2/3 items-center mt-4">
-                                    <p className="text-base text-center text-zinc-800 dark:text-slate-200 font-display w-1/2">
-                                        Hello I’m Nathan 👋
+                                <div className="flex w-5/6 2xl:w-2/3 items-center mt-4">
+                                    <p className="text-base text-center text-zinc-800 dark:text-slate-200 font-display mx-10 w-2/3 2xl:w-1/2">
+                                        Hello I’m Nathan <WavingHand motion={true} size='1.25rem' type={3}/>
                                         <br /><br />
                                         I’m a passionate and aspiring software developer with a keen interest in developing creative and intuitive digital landscapes. I’ve dedicated myself to building a strong foundation in modern web technologies such as JavaScript, TypeScript, React, Django and Next.JS. 
                                         <br /><br />
@@ -57,7 +59,7 @@ export default function About() {
                                         <br />
                                         Currently, I’m focused on building my portfolio with meaningful projects that showcase my abilities, as well as constantly learning new ways to create my ideas. I’m excited about the opportunity to collaborate with others and bring innovative ideas to life in a forward-thinking and determined environment.
                                     </p> 
-                                    <img src="/profile.jpg" className="w-[200px] hover:scale-150 transition ease-in-out rounded-full mx-auto" />
+                                    <img src="/profile.jpg" className="w-[300px] hover:scale-150 transition ease-in-out rounded-full mx-auto" />
                                 </div>  
                             </div>
                             <div className="flex flex-col items-center lg:hidden">
@@ -68,23 +70,23 @@ export default function About() {
                                         </h1>       
                                     </div>           
                                 </LampContainerSmall> 
-                                <div className="flex flex-col w-5/6 space-y-10 -translate-y-28">
+                                <div className="flex flex-col w-5/6 space-y-16 -translate-y-28">
                                     <p className="mt-8 text-sm text-center text-zinc-800 dark:text-slate-200 font-display">
-                                    Hello I’m Nathan 👋
-                                        <br /><br />
+                                    Hello I’m Nathan <WavingHand motion={true} size='1rem' type={3}/>
+                                    <br /><br />
                                         I’m a passionate and aspiring software developer with a keen interest in developing creative and intuitive digital landscapes. I’ve dedicated myself to building a strong foundation in modern web technologies such as JavaScript, TypeScript, React, Django and Next.JS. 
                                         <br /><br />
                                         I get a thrill from being challenged and working hard to solve problems using a logical and methodical approach. I am a great listener, with a friendly, approachable and down-to-earth style whilst maintaining professionalism. I’m project-driven and enjoy achieving success working towards goals
                                         <br />
                                         <br />
                                         Currently, I’m focused on building my portfolio with meaningful projects that showcase my abilities, as well as constantly learning new ways to create my ideas. I’m excited about the opportunity to collaborate with others and bring innovative ideas to life in a forward-thinking and determined environment.                                    </p> 
-                                    <img src="/profile.jpg" className="w-[200px] hover:scale-150 transition ease-in-out rounded-full mx-auto" />
+                                    <img src="/profile.jpg" className="w-[300px] transition ease-in-out rounded-full mx-auto" />
                                 </div>           
                             </div>
                         </div>
                     ) : (
                         <div className="relative z-50 flex flex-col items-center pt-48 md:pt-40 lg:pt-48">
-                            <div className="hidden lg:flex flex-col w-2/3">
+                            <div className="hidden lg:flex flex-col w-5/6 2xl:w-2/3">
                                 <h1 className="mt-6 font-bold mb-24 bg-gradient-to-br from-orange-900 to-orange-500 py-4 bg-clip-text text-center text-4xl font-display text-transparent lg:text-7xl">
                                     About Me
                                 </h1>   
@@ -98,16 +100,16 @@ export default function About() {
                                     ease: "easeInOut",
                                     }}
                                 >
-                                    <p className=" text-center text-zinc-800 dark:text-slate-200 font-display w-1/2">
-                                    Hello I’m Nathan 👋
-                                        <br /><br />
+                                    <p className=" text-center text-zinc-800 dark:text-slate-200 font-display mx-10 w-2/3 2xl:w-1/2">
+                                    Hello I’m Nathan <WavingHand motion={true} size='1.25rem' type={3}/>
+                                    <br /><br />
                                         I’m a passionate and aspiring software developer with a keen interest in developing creative and intuitive digital landscapes. I’ve dedicated myself to building a strong foundation in modern web technologies such as JavaScript, TypeScript, React, Django and Next.JS. 
                                         <br /><br />
                                         I get a thrill from being challenged and working hard to solve problems using a logical and methodical approach. I am a great listener, with a friendly, approachable and down-to-earth style whilst maintaining professionalism. I’m project-driven and enjoy achieving success working towards goals
                                         <br />
                                         <br />
                                         Currently, I’m focused on building my portfolio with meaningful projects that showcase my abilities, as well as constantly learning new ways to create my ideas. I’m excited about the opportunity to collaborate with others and bring innovative ideas to life in a forward-thinking and determined environment.                                    </p> 
-                                    <img src="/profile.jpg" className="w-[200px] hover:scale-150 transition ease-in-out rounded-full mx-auto" />
+                                    <img src="/profile.jpg" className="w-[300px] hover:scale-150 transition ease-in-out rounded-full mx-auto" />
                                 </div>     
                             </div> 
                             <div className="flex lg:hidden justify-center mt-16">
@@ -115,9 +117,9 @@ export default function About() {
                                     <h1 className="mt-8 font-bold bg-gradient-to-br from-orange-900 to-orange-500 py-4 bg-clip-text text-center text-4xl font-display text-transparent md:text-7xl">
                                         About Me
                                     </h1>   
-                                    <div className="flex flex-col space-y-10">
+                                    <div className="flex flex-col space-y-16">
                                         <p className="mt-8 text-sm text-center text-zinc-800 dark:text-slate-200 font-display">
-                                        Hello I’m Nathan 👋
+                                        Hello I’m Nathan <WavingHand motion={true} size='1rem' type={3}/>
                                         <br /><br />
                                         I’m a passionate and aspiring software developer with a keen interest in developing creative and intuitive digital landscapes. I’ve dedicated myself to building a strong foundation in modern web technologies such as JavaScript, TypeScript, React, Django and Next.JS. 
                                         <br /><br />
@@ -125,7 +127,7 @@ export default function About() {
                                         <br />
                                         <br />
                                         Currently, I’m focused on building my portfolio with meaningful projects that showcase my abilities, as well as constantly learning new ways to create my ideas. I’m excited about the opportunity to collaborate with others and bring innovative ideas to life in a forward-thinking and determined environment.                                        </p> 
-                                        <img src="/profile.jpg" className="w-[200px] hover:scale-150 transition ease-in-out rounded-full mx-auto" />
+                                        <img src="/profile.jpg" className="w-[300px] hover:scale-150 transition ease-in-out rounded-full mx-auto" />
                                     </div>     
                                 </div>           
                             </div>
