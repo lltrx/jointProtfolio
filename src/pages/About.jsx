dcef71479b58c2ea9a3daf4e6985c735db9dc211
Nathan Hutchison <nathanhutchison02@gmail.com>
2024-07-29 11:53:47 +0100

    fix(project): Removed waving hand and fix spacing issue on work experience section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,6 @@
 import { LampContainer } from "../components/lamp";
 import { LampContainerSmall } from "../components/lampSmall";
 import { useState, useEffect } from "react"
-import { WavingHand } from '@tensaye-yuan/react-waving-hand'
-import '@tensaye-yuan/react-waving-hand/dist/index.css'
 
 
 export default function About() {
@@ -37,7 +35,7 @@ export default function About() {
 
 
     return (
-        <div className="flex flex-col w-full items-center mb-48 md:p-20 lg:pt-0 2xl:pt-20 lg:-mt-[80px] 2xl:-mt-[100px] space-y-4 h-screen">
+        <div className="flex flex-col w-full items-center mb-44 lg:mb-96 2xl:mb-44 md:p-20 lg:pt-0 2xl:pt-20 lg:-mt-[80px] 2xl:-mt-[100px] space-y-4 h-screen">
             <div className="z-10 w-full flex flex-col justify-center items-center">
                 {
                     dark ? (
@@ -50,7 +48,7 @@ export default function About() {
                                 </LampContainer>
                                 <div className="flex w-5/6 2xl:w-2/3 items-center mt-4">
                                     <p className="text-base text-center text-zinc-800 dark:text-slate-200 font-display mx-10 w-2/3 2xl:w-1/2">
-                                        <a className="text-2xl">Hello I’m Nathan! </a><WavingHand motion={true} size='3rem' type={3}/>
+                                        <a className="text-2xl">Hello I’m Nathan! </a>
                                         <br /><br />
                                         I’m a passionate and aspiring software developer with a keen interest in developing creative and intuitive digital landscapes. I’ve dedicated myself to building a strong foundation in modern web technologies such as JavaScript, TypeScript, React, Django and Next.JS. 
                                         <br /><br />
@@ -72,7 +70,7 @@ export default function About() {
                                 </LampContainerSmall> 
                                 <div className="flex flex-col w-5/6 space-y-16 -translate-y-28">
                                     <p className="mt-8 text-sm text-center text-zinc-800 dark:text-slate-200 font-display">
-                                    <a className="text-2xl">Hello I’m Nathan! </a><WavingHand motion={true} size='2rem' type={3}/>
+                                    <a className="text-2xl">Hello I’m Nathan! </a>
                                     <br /><br />
                                         I’m a passionate and aspiring software developer with a keen interest in developing creative and intuitive digital landscapes. I’ve dedicated myself to building a strong foundation in modern web technologies such as JavaScript, TypeScript, React, Django and Next.JS. 
                                         <br /><br />
@@ -87,12 +85,12 @@ export default function About() {
                     ) : (
                         <div className="relative z-50 flex flex-col items-center pt-48 md:pt-40 lg:pt-48">
                             <div className="hidden lg:flex flex-col w-5/6 2xl:w-2/3">
-                                <h1 className="mt-6 font-bold mb-16 bg-gradient-to-br from-orange-900 to-orange-500 pt-4 pb-6 bg-clip-text text-center text-4xl font-display text-transparent lg:text-7xl">
+                                <h1 className="mt-6 font-bold mb-16 bg-gradient-to-br from-orange-900 to-orange-500 pt-4 pb-8 bg-clip-text text-center text-4xl font-display text-transparent lg:text-7xl">
                                     About Me
                                 </h1>   
-                                <div className="flex items-center">
+                                <div className="flex items-center mt-4">
                                     <p className=" text-center text-zinc-800 dark:text-slate-200 font-display mx-10 w-2/3 2xl:w-1/2">
-                                    <a className="text-2xl">Hello I’m Nathan! </a><WavingHand motion={true} size='3rem' type={3}/>
+                                    <a className="text-2xl">Hello I’m Nathan! </a>
                                     <br /><br />
                                         I’m a passionate and aspiring software developer with a keen interest in developing creative and intuitive digital landscapes. I’ve dedicated myself to building a strong foundation in modern web technologies such as JavaScript, TypeScript, React, Django and Next.JS. 
                                         <br /><br />
@@ -100,7 +98,7 @@ export default function About() {
                                         <br />
                                         <br />
                                         Currently, I’m focused on building my portfolio with meaningful projects that showcase my abilities, as well as constantly learning new ways to create my ideas. I’m excited about the opportunity to collaborate with others and bring innovative ideas to life in a forward-thinking and determined environment.                                    </p> 
-                                    <img src="/profile.jpg" className="w-[300px] hover:scale-150 transition ease-in-out rounded-full mx-auto pt-3" />
+                                    <img src="/profile.jpg" className="w-[300px] hover:scale-150 transition ease-in-out rounded-full mx-auto" />
                                 </div>     
                             </div> 
                             <div className="flex lg:hidden justify-center mt-16">
@@ -110,7 +108,7 @@ export default function About() {
                                     </h1>   
                                     <div className="flex flex-col space-y-16">
                                         <p className="mt-8 text-sm text-center text-zinc-800 dark:text-slate-200 font-display">
-                                        <a className="text-2xl">Hello I’m Nathan! </a><WavingHand motion={true} size='2rem' type={3}/>
+                                        <a className="text-2xl">Hello I’m Nathan! </a>
                                         <br /><br />
                                         I’m a passionate and aspiring software developer with a keen interest in developing creative and intuitive digital landscapes. I’ve dedicated myself to building a strong foundation in modern web technologies such as JavaScript, TypeScript, React, Django and Next.JS. 
                                         <br /><br />
